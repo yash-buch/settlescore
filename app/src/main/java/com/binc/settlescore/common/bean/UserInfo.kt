98083f6c370d0500ef00897362fcb3be70e614b2
Yash <yash@Yashs-MacBook-Pro.local>
2019-12-05 20:05:07 +0530

    Adding kdoc for UserInfo class

--- app/src/main/java/com/binc/settlescore/common/bean/UserInfo.kt
+++ app/src/main/java/com/binc/settlescore/common/bean/UserInfo.kt
@@ -31,14 +31,22 @@ class UserInfo private constructor(@NotNull name: String, @NotNull phNumber: Str
 
     // TODO: username: field
 
+    /**
+     *  API to validate @param name
+     *  @param name the name of the user
+     */
     fun validateName(name: String): Boolean {
         return !name?.isNullOrBlank()
     }
 
+    /**
+     *  API to validate @param phNumber
+     *  @param phNumber the phone number of the user
+     */
     fun validatePhoneNumber(phNumber: String): Boolean {
         var isNumber = true
         for (char in phNumber) {
-            if(!char.isDigit()) {
+            if (!char.isDigit()) {
                 isNumber = false
                 break
             }
@@ -46,17 +54,27 @@ class UserInfo private constructor(@NotNull name: String, @NotNull phNumber: Str
         return !phNumber?.isNullOrBlank() and (phNumber.length == 10) and isNumber
     }
 
+    /**
+     *  API to validate @param upi
+     *  @param upi the upi id of the user
+     */
     fun validateUPI(upi: String): Boolean {
         return !upi?.isNullOrBlank() and upi.contains('@')
     }
 
+    /**
+     *  API to create UserInfo object
+     *  @param name
+     *  @param phNumber
+     *  @param upi
+     */
     companion object {
         fun createUserInfo(name: String, phNumber: String, upi: String): UserInfo {
             var userInfo = UserInfo(name, phNumber, upi)
-            if(userInfo.validateName(name) and
-                    userInfo.validatePhoneNumber(phNumber) and
-                    userInfo.validateUPI(upi))
-                return userInfo
+            if (userInfo.validateName(name) and
+                userInfo.validatePhoneNumber(phNumber) and
+                userInfo.validateUPI(upi)
+            ) return userInfo
             throw ParamIncorrectException()
         }
     }
