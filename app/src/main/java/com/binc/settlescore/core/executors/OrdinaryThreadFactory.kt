611311831ecc4480f82c38a826c93950a633a724
Yash <yash@Yashs-MacBook-Pro.local>
2019-12-05 20:48:00 +0530

    Remove Threadpool implementation

--- app/src/main/java/com/binc/settlescore/core/executors/OrdinaryThreadFactory.kt
+++
@@ -1,9 +0,0 @@
-package com.binc.settlescore.core.executors
-
-class OrdinaryThreadFactory() : ThreadFactory() {
-    override fun newThread(r: Runnable?): Thread {
-        val thread = super.newThread(r)
-        thread.priority = 3
-        return thread
-    }
-}
