a09084815c0dbe2d4bfb6851079156c4844ba842
Yash <yash@Yashs-MacBook-Pro.local>
2020-07-19 01:13:44 +0530

    update baseusecase

--- app/src/main/java/com/binc/settlescore/domain/usecases/BaseUseCase.kt
+++ app/src/main/java/com/binc/settlescore/domain/usecases/BaseUseCase.kt
@@ -1,14 +1,10 @@
 package com.binc.settlescore.domain.usecases
 
 import com.binc.settlescore.domain.interactors.Result
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.withContext
 
 abstract class BaseUseCase<K> {
 
-    protected suspend fun execute(): Result<K> = withContext(Dispatchers.Main) {
-        return@withContext Result(doInForeground())
-    }
+    protected suspend fun execute(): Result<K> = Result(doInForeground())
 
     protected abstract suspend fun doInForeground(): K
 }
